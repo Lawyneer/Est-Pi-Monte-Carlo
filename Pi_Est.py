e9466b581b777b16a3dfa8c3d258fcec065c7b7f
GT-Engineer <65504372+gt-engineer@users.noreply.github.com>
2020-09-06 23:00:52 -0400

    Pi_Est.py
    Provided comments.

--- Pi_Est.py
+++ Pi_Est.py
@@ -3,28 +3,32 @@
 # Import packages and initialize variables
 import random # for the RNG
 
-circle_points = 0
-total_points = 0
-keep_going = 1
-
 for k in range(0,10):
-    circle_points = 0
-    total_points = 0
-    keep_going = 1
+    # Initialize variables for each loop
+    circle_points = 0 # Points inside a circle
+    total_points = 0  # Total points
+    keep_going = 1    # Termination criteria
 
-    while keep_going == 1:
-    #for i in range(POINTS**2):
-        rand_x = random.uniform(-1, 1)
-        rand_y = random.uniform(-1, 1)
-        dist = rand_x**2 + rand_y**2
+    while keep_going == 1:  # while loop to estimate pi
+        rand_x = random.uniform(-1, 1) # x coordinate
+        rand_y = random.uniform(-1, 1) # y coordinate
+        dist = rand_x**2 + rand_y**2   # distance from center of circle
         
+        # Determine if point landed inside circle
         if dist <= 1:
             circle_points+= 1
+        # end if
+        
+        # Increament total points
         total_points+= 1
-
+        
+        # Calculate Pi
         pi = 4* circle_points/total_points
-
+        
+        # Test for error
         if (pi < 3.142) & (pi > 3.140):
-            keep_going = 0
-            
+            keep_going = 0 # Set termination criteria when error small
+    
+    # Print estimate of Pi
     print("The estimate of Pi is: ", pi)
+    
